5923676e8b67c6d0286c7d70986f56d815b0851e
Mathieu Morue <mmorue@k0r2p8.42mulhouse.fr>
2022-11-18 16:59:34 +0100

    first push

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,17 @@
 /*   By: mmorue <mmorue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:09:23 by mmorue            #+#    #+#             */
-/*   Updated: 2022/11/17 17:16:06 by mmorue           ###   ########.fr       */
+/*   Updated: 2022/11/18 16:53:13 by mmorue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-#include  "list.c"
+#include "ft_list.c"
+#include "ft_itoa.c"
+#include "ft_hexa.c"
+#include "ft_hexa_maj.c"
 
-void	ft_readlst(p_list *start)
+void	ft_readlst(p_list *start)			//Lire la liste
 {
 	while (start->next != NULL)
 	{
@@ -23,7 +26,51 @@ void	ft_readlst(p_list *start)
 	printf("%s", start->content);
 }
 
-int	ft_string(char *str, p_list **start)
+int	ft_di(int i, p_list **start)			//Pour nombre decimal
+{
+	p_list	*new;
+	char	*str;
+
+	str = ft_itoa(i);
+	new = ft_lstnew_pf(str);
+	ft_lstadd_back_pf(start, new);
+	return (ft_strlen(str));
+}
+
+int	ft_u(unsigned int i, p_list **start)			//Pour unsigned int		
+{
+	p_list	*new;
+	char	*str;
+
+	str = ft_itoa_u(i);
+	new = ft_lstnew_pf(str);
+	ft_lstadd_back_pf(start, new);
+	return (ft_strlen(str));
+}
+
+int	ft_x(unsigned int i, p_list **start)			//Pour Hexa minuscule		
+{
+	p_list	*new;
+	char	*str;
+
+	str = ft_itoa_hexa(i);
+	new = ft_lstnew_pf(str);
+	ft_lstadd_back_pf(start, new);
+	return (ft_strlen(str));
+}
+
+int	ft_x_maj(unsigned int i, p_list **start)			//Pour Hexa Majuscule
+{
+	p_list	*new;
+	char	*str;
+
+	str = ft_itoa_hexa_maj(i);
+	new = ft_lstnew_pf(str);
+	ft_lstadd_back_pf(start, new);
+	return (ft_strlen(str));
+}
+
+int	ft_string(char *str, p_list **start)	//Pour afficher string
 {
 	p_list	*new;
 	char	*strn;
@@ -34,7 +81,7 @@ int	ft_string(char *str, p_list **start)
 	return (ft_strlen(strn));
 }
 
-int	ft_char(char c, p_list **start)
+int	ft_char(char c, p_list **start)			//Pour afficher char
 {
 	p_list	*new;
 	char	str[2];
@@ -58,11 +105,16 @@ int	ft_pars(char ftm, p_list **start, va_list arg)
 	else if (ftm == 's')
 		size = ft_string(va_arg(arg, char *), start);
 	//else if (ftm == 'p')
-	//else if (ftm == 'd')
-	//else if (ftm == 'i')
-	//else if (ftm == 'u')
-	//else if (ftm == 'x')
-	//else if (ftm == 'X')
+	else if (ftm == 'd')
+		size = ft_di(va_arg(arg, int), start);
+	else if (ftm == 'i')
+		size = ft_di(va_arg(arg, int), start);
+	else if (ftm == 'u')
+		size = ft_u(va_arg(arg, unsigned int), start);
+	else if (ftm == 'x')
+		size = ft_x(va_arg(arg, unsigned int), start);
+	else if (ftm == 'X')
+		size = ft_x_maj(va_arg(arg, unsigned int), start);
 	else if (ftm == '%')
 		size = ft_char(ftm, start);
 	return (size);
@@ -96,6 +148,7 @@ int	ft_printf(const char *fmt, ...)
 
 	start = 0;
 	va_start(arg, fmt);
+	va_end(arg);
 	ft_test(fmt, &start, arg);
 	ft_readlst(start);
 	return (0);
@@ -104,11 +157,12 @@ int	ft_printf(const char *fmt, ...)
 int	main()
 {
 	char	str[] = "salut";
-	
+	unsigned int		i;
+
+	i = 124363467;
 	printf("---- TEST VRAI ----\n");
-	printf("%%%s\n", str);
+	printf("%sokok%x\n", str, i);
 	printf("---- TEST MOI ----\n");
-	ft_printf("%%%s\n", str);
-	printf("---- TEST MOI ----\n");
+	ft_printf("%sokok%x\n",str, i);
 	return (0);
 }
--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: mmorue <mmorue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:10:08 by mmorue            #+#    #+#             */
-/*   Updated: 2022/11/17 16:58:07 by mmorue           ###   ########.fr       */
+/*   Updated: 2022/11/18 16:53:12 by mmorue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,16 @@ p_list	*ft_lstnew_pf(void *content);
 void	ft_lstadd_back_pf(p_list **lst, p_list *new);
 p_list	*ft_lstlast_pf(p_list *lst);
 int		ft_lstsize_pf(p_list *lst);
-
+char	*ft_itoa_hexa_maj(unsigned int n);
+char	*ft_itoa_hexa(unsigned int n);
+char	*ft_itoa_u(unsigned int n);
+int		ft_printf(const char *fmt, ...);
+int		ft_test(const char *ftm, p_list **start, va_list arg);
+int		ft_pars(char ftm, p_list **start, va_list arg);
+int		ft_char(char c, p_list **start);
+int		ft_string(char *str, p_list **start);
+int		ft_x_maj(unsigned int i, p_list **start);
+int		ft_x(unsigned int i, p_list **start);
+int		ft_u(unsigned int i, p_list **start);
+int		ft_di(int i, p_list **start);
 #endif
